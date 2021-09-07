7343ae231967f2050eb4db5132aebcc55533686c
Ameya Sinha <ameyaosd@gmail.com>
2021-09-07 19:37:42 +0530

    Add a DB

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+var mongodb = require('mongodb')
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // Basic Configuration
@@ -27,33 +29,60 @@ app.listen(port, function() {
 
 function validURL(str) {
   var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
   return !!pattern.test(str);
+}
+
+function shorterUrl() {
+  var text = "";
+  var possible =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+  for (var i = 0; i < 5; i++)
+  text += possible.charAt(Math.floor(Math.random() * possible.length));
+  
+  return text;
 }
 
 let urlCounter = 0;
 const store = new Map();
 
+const uri = process.env.MONGO_URI
+console.log("Uri is "+uri);
+mongoose.connect(uri, {serverSelectionTimeoutMS: 20000});
+
+const Schema = mongoose.Schema;
+const urlSchema = new Schema({
+  original_url : String,
+  short_url: String
+})
+const URL = mongoose.model("URL",urlSchema);
+
 app.post('/api/shorturl', (req,res, next) => {
   console.log(req.body.url);
-  if(validURL(req.body.url)) {
-    urlCounter++;
-    originalUrl = req.body.url;
-    store.set(parseInt(urlCounter), originalUrl);
-    next();
+  if(!validURL(req.body.url)) {
+    res.status(401).json({ error: 'invalid url' });
   } else {
-    res.json({ error: 'invalid url' });
+    next();
   }
 }, (req, res) =>{
-  res.json({ original_url : req.body.url, short_url : urlCounter});
+  const urlPair = new URL({original_url: req.body.url, short_url: shorterUrl()});
+  urlPair.save((err) => {
+    if(err) {
+      console.log("Error saving document");
+    }
+  })
+  res.json({ original_url : urlPair.original_url, short_url : urlPair.short_url});
 })
 
-app.get('/api/shorturl/:urlId',(req, res) => {
+app.get('/api/shorturl/:urlId', async (req, res) => {
   console.log("url ID = " + req.params.urlId);
+  const urlPair = await URL.findOne({short_url: req.params.urlId});
+  res.redirect(urlPair.original_url);
   // res.json({"url ": store.get(parseInt(req.params.urlId))});
-  res.redirect(store.get(parseInt(req.params.urlId)));
+  // res.redirect(store.get(parseInt(req.params.urlId)));
 })
